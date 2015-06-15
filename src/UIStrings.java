08136d9f64ae934d53dfb39011a06c6268f688ac
Syed Raza <razasyedh@gmail.com>
2015-06-15 08:35:51 -0400

    Update app strings

--- src/UIStrings.java
+++ src/UIStrings.java
@@ -3,24 +3,15 @@
  */
 public class UIStrings {
     // Tooltips
-    public static final String startButtonTip = "Add a start point";
-    public static final String endButtonTip = "Add an end point";
-    public static final String blockButtonTip = "Block a point";
-    public static final String noneButtonTip = "Reset a point";
-    public static final String navigateButtonTip = "Calculate the path between"
-                                                   + " the start and end"
-                                                   + " points";
-    public static final String resetButtonTip = "Reset all the points to an"
-                                                + " empty state";
+    public static final String startButtonTip = "Move the start point";
+    public static final String endButtonTip = "Move the end point";
+    public static final String blockButtonTip = "Block/Unblock a point";
+    public static final String resetButtonTip = "Clear all the blocked points";
 
     // Errors
     public static final String sameStartEndPoints = "The end point cannot be"
                                                     + " the same as the start"
                                                     + " point";
-    public static final String missingStartEndPoint = "Both a start and end"
-                                                      + " point must be chosen";
-    public static final String unreachableEndpoint = "The endpoint is"
-                                                     + " unreachable";
     public static final String blockedStartEndPoint = "The start and end"
                                                       + " points cannot be"
                                                       + " blocked";
