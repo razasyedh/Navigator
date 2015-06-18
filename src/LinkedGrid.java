40b14470b63a8811a672774bc839802a37488689
Syed Raza <razasyedh@gmail.com>
2015-06-17 20:14:15 -0400

    Move grid toString method to superclass

--- src/LinkedGrid.java
+++ src/LinkedGrid.java
@@ -67,42 +67,6 @@ public class LinkedGrid extends Grid {
         }
     }
 
-    /**
-     * Returns a string representation of the grid with numbered axes.
-     * <p>
-     * Note: The axes of the grid are switched, so the x coordinate would
-     * correspond to the vertical axis and vice-versa.
-     *
-     * @return The formatted grid as a string.
-     */
-    @Override
-    public String toString() {
-        StringBuilder result = new StringBuilder("\\Y");
-
-        // Column numbers
-        for (int j = 0; j < cols; j++) {
-            String colNum = String.format("%3s", j);
-            result.append(" " + colNum);
-        }
-        result.append("\nX _|\n");
-
-        for (int i = 0; i < rows; i++) {
-            // Row numbers
-            String row = String.format("%2s", i);
-            result.append(row + " ");
-
-            for (int j = 0; j < cols; j++) {
-                String node = grid[i][j].toString();
-                String valueString = String.format("%3s", node);
-
-                result.append(valueString  + " ");
-            }
-            result.append("\n");
-        }
-
-        return result.toString();
-    }
-
     public static void main(String[] args) {
         LinkedGrid test = new LinkedGrid(2, 5);
         test.getNode(0, 1).setValue(1);
